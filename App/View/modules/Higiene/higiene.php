945be4e5abc31fc4d5d219e75189f55b260cafbf
Marcos Vinicius <82465988+marcosviniciusjau@users.noreply.github.com>
2022-11-21 12:29:56 -0300

    Update higiene.php

--- App/View/modules/Higiene/higiene.php
+++ App/View/modules/Higiene/higiene.php
@@ -19,78 +19,50 @@
   </head>
   <body>
   
-<nav class="navbar navbar-light">
-  <div class="container-fluid">
-     <a href="/"><img src="View/Imagens/Header/logofundo.png" class="img-fluid" width="370" height="103">
-    <a class="navbar-brand" href="#">
 
-<div class="seila">
-<div id="wpp"><a href="https://web.whatsapp.com"><img src="View/Imagens/Header/whatsapp.png" width="40" height="40"></a></div>
-<div id="fb"><a href="https://www.facebook.com/Green-Life-113598344685626"><img src="View/Imagens/Header/facebook.png" width="40" height="40"></a></div>
-<div id="ig"><a href="https://www.instagram.com/greenlifevegan01/"><img src="View/Imagens/Header/instagram.png" width="40" height="40"></a></div>
-<div id="tel"><a href="https://web.whatsapp.com"><img src="View/Imagens/Header/phone-call.png" width="40" height="40"></a></div>
-<div id="email"><a href="https://mail.google.com/mail/"><img src="View/Imagens/Header/email.png" width="40" height="40"></a></div>
-</div>
-
-</a>
-  </div>
-
-  </nav>
-
-   <nav class="navbar navbar-light" style="background-color: #023418">
+  <nav class="navbar navbar-expand-md navbar-dark bg-success">
     <div class="container-fluid">
-       <a class="navbar-brand" href="promocoes">
-          <font face="Corbel Light" size="4.5" color="white">ﾠﾠﾠPROMOÇÕES</a>
-       
-      </a></font>
+      <a href="/"><img src="View/Imagens/Rodape/logorodape.png" class="navbar-brand" width="157" height="43"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="promocoes">Promoções</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="alimentacao">Alimentação</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="vestuario">Vestuário</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="higiene">Higiene</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="cosmeticos">Cosméticos</a>
+          </li>
+        </ul>
 
-     <nav class="navbar navbar-light" style="background-color: #023418">
-    
-    <div class="container-fluid">
-          <a class="navbar-brand" href="alimentacao">
-        
-         <font face="Corbel Light" size="4.5" color="white"> ALIMENTAÇÃO</a>
-       
-      </a></font>
-       <a class="navbar-brand" href="vestuario">
-       
-          <font face="Corbel Light" size="4.5" color="white">VESTUÁRIO</a>
-      </a></font>
-      <br>
-       <a class="navbar-brand" href="higiene">
-         
-         <font face="Corbel Light" size="4.5" color="white">HIGIENE</a>
-        
-      </a></font>
-          <a class="navbar-brand" href="cosmeticos">
-         
-          <font face="Corbel Light" size="4.5" color="white">COSMÉTICOS</a>
-         </a></font>
-        <form class="d-flex">
-        <input class="form-control me-4" type="search" placeholder="Pesquisar" aria-label="Search">
-       
-      <a class="navbar-brand" href="#">
-        <a href="login"><img src="View/Imagens/Header/bag.png" class="img-fluid" alt="" width="50" height="50" class="d-inline-block align-text-top">
-        
-        <br>
-      </a></font>
-
-      </form>
-       <a class="navbar-brand" href="#">
-         <a href="login"><img src="View/Imagens/Header/user.png" class="img-fluid" alt="" width="35" height="35" class="d-inline-block align-text-top">
-          
-      </a></font>
-      
+        <div class="text-end">
+          <a href="login_usuario"><button type="button" class="btn btn-outline-light me-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+              <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+            </svg> Login</button></a>
+          <a href="usuario/form"><button type="button" class="btn btn-light">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+              <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+            </svg> Cadastro</button></a>
+        </div>
+      </div>
     </div>
   </nav>
-  </nav>
+  <br>
 
-<br>
-
-    <div id="conteudo">
-  <table border="0" cellspacing="10" cellpadding="10" style="margin:0 auto" >
-   <tr><th colspan="10"><font size="10" style= "font-family: 'Corbel'"><center>── HIGIENE ──</center></font>
-   <div class="container text-center">
+  <font size="10" style= "font-family: 'Corbel'"><center>── ALIMENTAÇÃO ──</center></font><div class="container text-center">
     <div class="row">
     <?php foreach($model->rows as $item): ?>
       <div class="col">
