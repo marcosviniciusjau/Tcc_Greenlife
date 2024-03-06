cdfd787b22dcb11634a441592cacf89329de7353
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-03-06 14:12:27 -0300

    adjustments on security

--- App/config.php
+++ App/config.php
@@ -5,7 +5,8 @@ define('VIEWS', BASEDIR . '/App/View/modules/');
 define('UPLOADS', BASEDIR . '/App/View/Uploads/');
 define('PATH_VIEW', dirname(__FILE__, 2) . '/App/View/');
 
-$_ENV['db']['host'] = 'localhost:3307';
-$_ENV['db']['user'] = 'root';
-$_ENV['db']['pass'] = 'etecjau';
-$_ENV['db']['database'] = 'db_mvc';
+
+$_ENV['db']['host'] = $_ENV['DB_HOST'];
+$_ENV['db']['user'] = $_ENV['DB_USER'];
+$_ENV['db']['pass'] = $_ENV['DB_PASS'];
+$_ENV['db']['database'] = $_ENV['DB_DATABASE'];
